aae8d39b5e8b7612e685762b8bfe9bcce310a545
Ali Nobariasl <freeeeboy.gh@gmail.com>
2024-05-23 04:03:52 +0300

    fixing paging problem by adding two new function

--- config.py
+++ config.py
@@ -2,4 +2,5 @@
 config = {
     "google_api_key": "AIzaSyCtda1OJMuWGwsQkX-vjoITkEgYeTFJ7pM",
     "youtube_playlist_id": "PLa7VYi0yPIH0KbnJQcMv5N9iW8HkZHztH", 
+    "next_page_token": "EAAajgFQVDpDQVVpRURWQk5qVkRSVEV4TlVJNE56TTFPRVFvQVVqQTlaQ1QtOTc5QWxBQldrVWlRMmxLVVZSSFJUTldiR3h3VFVoc1VWTlZaM2RUTWtwMVUyeEdhbFJZV1RGVWFteHdWbnBvU1dFeGNFbGxibEpKUldkM1NYZFpaa3B2UVZsUlowOXFWV2gzVFNJ",
 }
--- youtubeTrack.py
+++ youtubeTrack.py
@@ -1,30 +1,49 @@
 import logging
 import sys
 import requests
+import json
 from config import config
 
 
+link = "https://www.googleapis.com/youtube/v3/playlistItems"
+
+def fetch_playlist_items_page(google_api_key,youtube_playlist_id, page_token= None):
+
+    response = requests.get(link, 
+        params={
+            "key" : google_api_key, 
+            "playlistId": youtube_playlist_id ,
+            "part": "contentDetails",
+            "pageToken": page_token,
+    })
+    
+    payload =json.loads(response.text)
+    logging.debug("GOT %s", payload)
+    return payload
+
+
+def fetch_playlist_items(google_api_key,youtube_playlist_id, page_token= None):
+    
+    payload = fetch_playlist_items_page(google_api_key,youtube_playlist_id, page_token)
+    
+    yield from payload["items"]
+    next_page_token = payload.get("nextPageToken")
+    if next_page_token is not None:
+        yield from fetch_playlist_items(google_api_key,youtube_playlist_id, next_page_token)
 
 
 def main():
     logging.info("Start ...")
-    link = "https://www.googleapis.com/youtube/v3/playlistItems?"
     google_api_key = config["google_api_key"]
     youtube_playlist_id = config["youtube_playlist_id"]
     
-    resonse = requests.get(link, params={
-        "key" : google_api_key, 
-        "playlistId": youtube_playlist_id ,
-        "part": "contentDetails",
-    })
-    
-    logging.debug("GOT %s", resonse.text )
-
+    for v_item in fetch_playlist_items(google_api_key,youtube_playlist_id):
+        logging.info("GOT %s", v_item)
 
 
 
 if __name__=='__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     sys.exit(main())
     
     
